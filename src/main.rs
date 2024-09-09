4f214cb492a67ea346089a3e93710a3eb26bf32c
tomeichlersmith <eichl008@umn.edu>
2024-09-08 19:10:43 -0500

    mock up CLI with clap in main
    figured I'd leave it there so folks can have a CLI method of drafting the command
    they want to eventually put into the book for injection.
    
    --help method works right now but nothing else.

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,46 @@ fn main() {
 
     if let Some(sub_args) = matches.subcommand_matches("supports") {
         handle_supports(sub_args);
+    } else if let Some(sub_args) = matches.subcommand_matches("cmdrun") {
+        CmdRun::run_cmdrun(sub_args, ".", false);
     } else if let Err(e) = handle_preprocessing() {
         eprintln!("{e}");
         process::exit(1);
     }
+}
+
+fn make_cmdrun_parser() -> Command {
+    Command::new("cmdrun")
+        .about("test run a command before putting it in a book")
+        .arg(
+            Arg::new("expect-return-code")
+            .help("require the specific return code N")
+            .long("expect-return-code")
+            .conflicts_with("strict")
+            .conflicts_with("exit-code-short")
+            .num_args(1)
+            .value_name("N")
+        ).arg(
+            Arg::new("strict")
+            .help("require command to return the successful exit code 0")
+            .long("strict")
+            .conflicts_with("expect-return-code")
+            .conflicts_with("exit-code-short")
+            .action(clap::ArgAction::SetTrue)
+        ).arg(
+            Arg::new("exit-code-short")
+            .help("require the specific exit code N")
+            .conflicts_with("expect-return-code")
+            .conflicts_with("strict")
+            .value_name("-N")
+            .allow_negative_numbers(true)
+            .value_parser(..=0)
+        ).arg(
+            Arg::new("cmd")
+            .help("command whose output will be injected into book")
+            .num_args(1..)
+            .trailing_var_arg(true)
+        )
 }
 
 fn make_app() -> Command {
@@ -26,6 +62,8 @@ fn make_app() -> Command {
             Command::new("supports")
                 .arg(Arg::new("renderer").required(true))
                 .about("Check whether a renderer is supported by this preprocessor"),
+        ).subcommand(
+            make_cmdrun_parser()
         )
 }
 
